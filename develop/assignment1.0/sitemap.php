79de2ecfe94e37151f7f6812b4b04b20049a334f
Jacob M Dubois <jmdubois@silk3.uvm.edu>
2015-09-19 13:36:05 -0400

    intial

---
+++ develop/assignment1.0/sitemap.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+    <head>
+        <title>Jacob Dubois - CS008</title>
+        <meta charset="utf-8">
+        <meta content="Jacob Dubois" name="author">
+        <meta content="Index file for CS008" name="description">
+        <meta content="CS008, Jacob Dubois, Web Design" name="keywords">
+        <link media="screen" type="text/css" href="style.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <h1>Jacob Dubois</h1>
+        <p><a href="assignment1.0/">Assignment 1.1</a></p>
+        
+
+
+    </body>
+</html>
